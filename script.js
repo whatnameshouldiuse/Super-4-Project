c2c5d999aee9d2ae0badc0be6d58f094eec1cd32
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-09-14 20:47:14 -0500

    updated js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // GET https://youtube.googleapis.com/youtube/v3/playlists?key=[YOUR_API_KEY] HTTP/1.1
-
+// fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?key=AIzaSyBniUrdA_ATDmJ6H5XjB3ByFZKSnmMqp38&playlistId=PLvahqwMqN4M2euuB96bo7rnVMcakCkIRZ")
 fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?key=AIzaSyBniUrdA_ATDmJ6H5XjB3ByFZKSnmMqp38&playlistId=PLvahqwMqN4M2euuB96bo7rnVMcakCkIRZ")
     .then(function (response) {
         return response.json();
@@ -7,3 +7,5 @@ fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?key=AIzaSyBniUrdA
     .then(function (data){
         console.log(data)
     })
+
+    
